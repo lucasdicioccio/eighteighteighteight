3d49a963f7c6de453915348a1de36fe87607ed62
Lucas DiCioccio <lucas@dicioccio.fr>
2017-08-24 21:58:09 +0000

    Save progress on Aeson instances and export cleanup.

--- src/Network/EightEightEightEight.hs
+++ src/Network/EightEightEightEight.hs
@@ -1,6 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Network.EightEightEightEight where
+module Network.EightEightEightEight (
+    connect
+  , query
+  -- * types
+  , EightEightEightEight
+  , Eight888Error
+  , Name
+  , RRType(..)
+  , EDNSSubnet(..)
+  , IPText
+  , Mask
+  , Reply(..)
+  , Question(..)
+  , Record(..)
+  ) where
 
 import           Control.Concurrent
 import           Control.Concurrent.Async
@@ -9,23 +23,97 @@ import           Data.Aeson
 import           Data.Default.Class (def)
 import           Data.Monoid ((<>))
 import           Data.String.Conversions
-import           Data.Text
+import           Data.Text as Text
 import           Network.HTTP2
 import           Network.HTTP2.Client
 import           Network.HTTP2.Client.Helpers
 import qualified Network.TLS as TLS
 import qualified Network.TLS.Extra.Cipher as TLS
 
+data Question = Question {
+    questionName :: Name
+  , questionType :: Int
+  } deriving (Eq, Ord, Show)
+
+instance FromJSON Question where
+    parseJSON = withObject "Question" $ \v ->
+        Question <$> v .: "name"
+                 <*> v .: "type"
+
+data Record = Record {
+    recordName :: Name
+  , recordType :: Int
+  , recordTTL  :: Int
+  , recordData :: Text
+  } deriving (Eq, Ord, Show)
+
+instance FromJSON Record where
+    parseJSON = withObject "Record" $ \v ->
+        Record <$> v .: "name"
+               <*> v .: "type"
+               <*> v .: "TTL"
+               <*> v .: "data"
+
+data Reply = Reply {
+    replyStatus :: Int
+  , replyTC :: Bool -- ^ truncated
+  , replyRD :: Bool
+  , replyRA :: Bool
+  , replyAD :: Bool
+  , replyCD :: Bool
+  , replyQuestions :: [Question]
+  , replyAnswers   :: [Record]
+  , replyComment   :: Maybe Text
+  , replyAuthority :: Maybe [Record]
+  } deriving (Eq, Ord, Show)
+
+instance FromJSON Reply where
+    parseJSON = withObject "Reply" $ \v ->
+        Reply <$> v .: "Status"
+              <*> v .: "TC"
+              <*> v .: "RD"
+              <*> v .: "RA"
+              <*> v .: "AD"
+              <*> v .: "CD"
+              <*> v .: "Question"
+              <*> v .: "Answer"
+              <*> v .:? "Comment"
+              <*> v .:? "Authority"
+
 type QueryParams = Text
+
 type Name = Text
+
 type IPText = Text
+
 type Mask = Int
+
 data EDNSSubnet = EDNSSubnet !IPText !Mask
 
 data RRType = A | AAAA | CNAME | MX | ANY | Specified Text
 
 queryParams :: RRType -> Maybe EDNSSubnet -> Name -> QueryParams
-queryParams rr edns name = "name=" <> name
+queryParams rr (Just edns) name = Text.intercalate "&"
+  [ "name=" <> name
+  , "type=" <> rrtoType rr
+  , "_edns_client_subnet=" <> ednsToText edns
+  ]
+queryParams rr Nothing name = Text.intercalate "&"
+  [ "name=" <> name
+  , "type=" <> rrtoType rr
+  ]
+
+ednsToText :: EDNSSubnet -> Text
+ednsToText (EDNSSubnet ipTxt mask) =
+    ipTxt <> "/" <> (convertString $ show mask)
+
+rrtoType :: RRType -> Text
+rrtoType A = "A"
+rrtoType AAAA = "AAAA"
+rrtoType CNAME = "CNAME"
+rrtoType MX = "MX"
+rrtoType ANY = "ANY"
+rrtoType (Specified t) = t
 
 queryPath :: RRType -> Maybe EDNSSubnet -> Name -> QueryParams
 queryPath rr edns name = "/resolve?" <> queryParams rr edns name
@@ -33,51 +121,33 @@ queryPath rr edns name = "/resolve?" <> queryParams rr edns name
 type Eight888Error = Text
 
 data EightEightEightEight = EightEightEightEight {
-    query :: RRType -> Maybe EDNSSubnet -> Name -> IO (Either Eight888Error StreamResponse)
+    query :: RRType -> Maybe EDNSSubnet -> Name -> IO (Either Eight888Error Reply)
   }
 
-tls :: TLS.ClientParams
-tls = TLS.ClientParams {
-      TLS.clientWantSessionResume    = Nothing
-    , TLS.clientUseMaxFragmentLength = Nothing
-    , TLS.clientServerIdentification = ("127.0.0.1", "")
-    , TLS.clientUseServerNameIndication = True
-    , TLS.clientShared               = def
-    , TLS.clientHooks                = def { TLS.onServerCertificate = \_ _ _ _ -> return []
-                                           }
-    , TLS.clientSupported            = def { TLS.supportedCiphers = TLS.ciphersuite_default }
-    , TLS.clientDebug                = def
-    }
-
-connect :: IO EightEightEightEight
-connect = do
+connect :: TLS.ClientParams -> IO EightEightEightEight
+connect tls = do
     conn <- newHttp2Client "dns.google.com" 443 4096 4096 tls []
     _addCredit (_incomingFlowControl conn) 10000000 -- 10Mio
-    _ <- async $ forever $ do
-            updated <- _updateWindow $ _incomingFlowControl conn
-            threadDelay 100000 -- 100ms
-    let query :: RRType -> Maybe EDNSSubnet -> Name -> IO (Either Eight888Error StreamResponse)
+    let query :: RRType -> Maybe EDNSSubnet -> Name -> IO (Either Eight888Error Reply)
         query rr edns name = do
+            let path = convertString $ queryPath rr edns name
+            print path
             let hdrs = [ (":method", "GET")
                        , (":scheme", "https")
                        , (":authority", "dns.google.com")
-                       , (":path", convertString $ queryPath rr edns name)
+                       , (":path", path)
                        ]
             val <- withHttp2Stream conn $ \stream ->
                 StreamDefinition (headers stream hdrs setEndStream)
                                  (\fcIN fcOUT -> fromStreamResult <$> waitStream stream fcIN)
+            _ <- _updateWindow $ _incomingFlowControl conn
             return $ case val of
                 Left _  ->
                     Left "too much concurrency"
                 Right (Left err) -> 
                     Left $ convertString $ show err
-                Right (Right x) -> 
-                      (Right x)
+                Right (Right (status,dat)) ->  
+                    let rec = eitherDecode (convertString dat) :: Either String Reply
+                    in either (Left . convertString) Right rec
 
     return $ EightEightEightEight query
-
-defaultMain :: IO ()
-defaultMain = do
-    eight888 <- connect
-    query eight888 A Nothing "haskell.org" >>= print
-    query eight888 A Nothing "cloud.google.com" >>= print
